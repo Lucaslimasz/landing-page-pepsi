5175cf56aa2f7872be030d0b6a354f5121d9be46
Lucaslimasz <id.lucas@outlook.com.br>
2022-03-17 17:53:01 -0300

    url corrections

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function handleBackgroundColor(color) {
 
   if (color === "blue") {
     background.style.background = "#0062be";
-    image.src = "/assets/pepsi-blue.png";
+    image.src = "assets/pepsi-blue.png";
   } else if (color === "red") {
     background.style.background = "#e60c2c";
-    image.src = "/assets/pepsi-gray.png";
+    image.src = "assets/pepsi-gray.png";
   } else {
     background.style.background = "#1f1f1f";
-    image.src = "/assets/pepsi-black.png";
+    image.src = "assets/pepsi-black.png";
   }
 }
